8a07d5f0d5ee6dc23f7ccbcc23ab926332db5ea3
Miss97 <649791555@qq.com>
2020-06-27 19:37:30 +0800

    e-mental project

---
+++ src/main/java/com/emental/EmentalApplication.java
@@ -0,0 +1,13 @@
+package com.emental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emental/EmentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
